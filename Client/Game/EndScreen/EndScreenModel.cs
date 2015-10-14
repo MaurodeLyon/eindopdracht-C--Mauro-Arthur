4404fd7af85ccc6b40e0d9e1b30b3cc519657f66
Mauro de Lyon <maurodelyon@gmail.com>
2015-10-15 00:10:44 +0200

    Added EndScreen

---
+++ Client/Game/EndScreen/EndScreenModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.EndScreen
+{
+    class EndScreenModel
+    {
+        private EndScreenView endScreenView;
+
+        public EndScreenModel(EndScreenView endScreenView)
+        {
+            this.endScreenView = endScreenView;
+        }
+    }
+}
---
+++ Client/Game/EndScreen/EndScreenView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game.EndScreen
+{
+    public partial class EndScreenView : Form
+    {
+        private EndScreenModel endScreemModel;
+
+        public EndScreenView()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;    //remove borders
+            this.TopMost = true;                            //set form to the front
+            this.Bounds = Screen.PrimaryScreen.Bounds;      //set fullscreen 
+            endScreemModel = new EndScreenModel(this);
+        }
+    }
+}
